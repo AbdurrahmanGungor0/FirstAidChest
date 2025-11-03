27c91d87125b9bbf97e4e16d0f7937b7641f8b07
Abdurrahman Güngör <abdurrahman.gungor@kariyer.net>
2025-11-03 12:05:19 +0300

    initialize project structure with initial files and configurations

--- FirstAidChest.Hybrid/www/js/controllers/inventory-controller.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by abdur on 22.12.2015.
- */
-
-'use strict';
-
-app.controller('InventoryController',['$scope','medicamentFactory', function ($scope,medicamentFactory) {
-//$scope.inventoryList=medicamentFactory.get
-}]);
--- FirstAidChest.Hybrid/www/js/controllers/settings-controller.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by abdur on 22.12.2015.
- */
-
-'use strict';
-
-app.controller('SettingsController',['$scope',function($scope){
-}]);
